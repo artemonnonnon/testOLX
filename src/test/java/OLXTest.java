f67c348f49908670a1ecc2dc0676e3f728ce7cbf
Artem <kharchenko_artem@ukr.net>
2017-04-13 12:02:32 +0300

    implemented login

--- src/test/java/OLXTest.java
+++ src/test/java/OLXTest.java
@@ -1,16 +1,31 @@
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OLXTest {
 
-    @BeforeClass public static void setup() {
+    WebDriver driver;
+
+    @Before public void setup() {
         System.setProperty("webdriver.chrome.driver", "seleniumDrivers/chromedriver.exe");
+        driver = new ChromeDriver();
     }
 
     @Test public void testNewAd() {
-        WebDriver driver = new ChromeDriver();
+        login("userName", "password");
         driver.get("https://www.olx.ua");
+
+        // add new ad
+    }
+
+    void login(String userName, String password) {
+        // TODO check if already logged in
+        driver.get("https://www.olx.ua");
+        driver.findElement(By.id("topLoginLink")).click();
+        driver.findElement(By.id("userEmail")).sendKeys(userName);
+        driver.findElement(By.id("userPass")).sendKeys(password);
+        driver.findElement(By.id("se_userLogin")).click();
     }
 }
